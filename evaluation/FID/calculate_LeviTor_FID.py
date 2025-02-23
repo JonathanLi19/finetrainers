5c2553d8aa558e6742468138e27f5913b8a75687
quanhaoli <quanhaol@163.com>
2025-02-23 01:57:33 +0000

    [quanhao] update evaluation

--- evaluation/FID/calculate_LeviTor_FID.py
+++ evaluation/FID/calculate_LeviTor_FID.py
@@ -7,8 +7,8 @@ import cv2
 
 
 # 读取原始 CSV 文件中的 videoid 列
-input_csv = '/datadrive2/lqh/Tora/sat/testset/final_testset.csv'
-output_csv = 'evaluation/FID/results/Tora.csv'
+input_csv = 'testset/final_testset.csv'
+output_csv = 'evaluation/FID/results/LeviTor.csv'
 
 # 存储 FID 值的结果
 fid_results = []
@@ -39,7 +39,7 @@ def calculate_fid(video_id):
         "python",
         "-m",
         "pytorch_fid",
-        f"/datadrive2/lqh/generated_videos_as_images/Tora/{video_id}",
+        f"/datadrive2/lqh/generated_videos_as_images/LeviTor/{video_id}",
         f"/datadrive2/lqh/testset_data/video_as_images/{video_id}/{video_id}.npz"
     ]
     result = subprocess.run(command, capture_output=True, text=True)
@@ -65,11 +65,11 @@ with open(input_csv, 'r') as infile:
 
     for row in reader:
         video_id = row['videoid']
-        output_path = f"/datadrive2/lqh/Tora/sat/samples/testset/{video_id}.mp4"
+        output_path = f"/datadrive2/lqh/LeviTor/output/output_videos_mp4/{video_id}.mp4"
 
         # 检查视频文件是否存在以及是否已经处理过
         if os.path.exists(output_path) and video_id not in existing_fid_results:
-            save_dir = f"/datadrive2/lqh/generated_videos_as_images/Tora/{video_id}"
+            save_dir = f"/datadrive2/lqh/generated_videos_as_images/LeviTor/{video_id}"
 
             if not os.path.exists(save_dir):
                 # 提取视频的每一帧
--- evaluation/FID/calculate_mean_FID.py
+++ evaluation/FID/calculate_mean_FID.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 读取 CSV 文件
-file_path = 'evaluation/FID/results/mask_condition.csv'
+file_path = 'evaluation/FID/results/LeviTor.csv'
 df = pd.read_csv(file_path)
 
 # 计算 'fid_value' 列的平均值
@@ -11,5 +11,6 @@ average_fid = df['fid_value'].mean()
 print(f'FID列的平均值是: {average_fid}')
 
 # DragAnything: 146.3747869303462
+# Tora: 144.4946397723322
 # box_condition: 114.31806044737142
 # mask_condition: 105.64298297478429
