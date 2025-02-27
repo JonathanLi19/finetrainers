de0aea9554dde25446edb5fe7343db792de6d755
quanhaoli <quanhaol@163.com>
2025-02-27 05:49:50 +0000

    [quanhao] update

--- training/cogvideox/ablation_scripts/inference/README.md
+++ training/cogvideox/ablation_scripts/inference/README.md
@@ -1,5 +1,10 @@
 # Ablation Inference Scripts
 ## Dataset Ablation
-1. 执行training/cogvideox/ablation_scripts/inference/inference_box_dataset_ablation_controlnet_I2V.sh
+1. 执行training/cogvideox/ablation_scripts/inference/dataset_ablation/inference_box_dataset_ablation_controlnet_I2V_1.sh (一共四个)
 2. perception_head_path, controlnet_path替换为本地路径
-3. --validation_args_csv 分别替换为四个part的路径(例如training/cogvideox/validation_args/testset/box_dataset_ablation/part/final_testset_part_1.csv), 然后依次执行脚本
+## Segment Loss Ablation
+1. 执行training/cogvideox/ablation_scripts/inference/segment_loss_ablation/inference_1.sh (一共四个)
+2. perception_head_path, controlnet_path替换为本地路径
+## Progressive Ablation
+1. 执行training/cogvideox/ablation_scripts/inference/progressive_ablation/inference_1.sh (一共四个)
+2. perception_head_path, controlnet_path替换为本地路径
---
+++ training/cogvideox/ablation_scripts/inference/progressive_ablation/inference_4.sh
@@ -0,0 +1,21 @@
+export TORCH_LOGS="+dynamo,recompiles,graph_breaks"
+export TORCHDYNAMO_VERBOSE=1
+export WANDB_MODE="offline"
+export NCCL_P2P_DISABLE=1
+export TORCH_NCCL_ENABLE_MONITORING=0
+export TOKENIZERS_PARALLELISM=False
+export CUDA_VISIBLE_DEVICES=3
+MODEL_PATH="THUDM/CogVideoX-5b-I2V"
+perception_head_path="checkpoints/progressive_ablation/checkpoint-6500/perception_head-checkpoint-6500.pt"
+controlnet_path="checkpoints/progressive_ablation/checkpoint-6500/controlnet-checkpoint-6500.pt"
+
+python training/cogvideox/I2V_inference_controlnet.py \
+    --pretrained_model_name_or_path  $MODEL_PATH \
+    --pretrained_controlnet_path $controlnet_path \
+    --pretrained_perception_head_path $perception_head_path \
+    --validation_args_csv  "training/cogvideox/validation_args/testset/box_progressive_ablation/part/final_testset_part_4.csv" \
+    --num_validation_videos 1 \
+    --seed 42 \
+    --height 480 \
+    --width 720 \
+    --use_perception_head
