44c635e4f8050a126cfd65fe493af6822cb3d48e
quanhaoli <quanhaol@163.com>
2025-03-01 05:44:53 +0000

    [quanhao] update

--- training/cogvideox/ablation_scripts/inference/segment_loss_ablation/inference_1.sh
+++ training/cogvideox/ablation_scripts/inference/segment_loss_ablation/inference_1.sh
@@ -4,14 +4,14 @@ export WANDB_MODE="offline"
 export NCCL_P2P_DISABLE=1
 export TORCH_NCCL_ENABLE_MONITORING=0
 export TOKENIZERS_PARALLELISM=False
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 MODEL_PATH="THUDM/CogVideoX-5b-I2V"
-controlnet_path="checkpoints/box_ablation_segment/checkpoint-6500.pt"
+controlnet_path="checkpoints/box_ablation_segment/checkpoint-1500.pt"
 
 python training/cogvideox/I2V_inference_controlnet.py \
     --pretrained_model_name_or_path  $MODEL_PATH \
     --pretrained_controlnet_path $controlnet_path \
-    --validation_args_csv  "training/cogvideox/validation_args/DAVIS/box_segment_ablation/testset_DAVIS.csv" \
+    --validation_args_csv  "training/cogvideox/validation_args/DAVIS/box_segment_ablation/remote_machine/step1500.csv" \
     --num_validation_videos 1 \
     --seed 42 \
     --height 480 \
--- training/cogvideox/ablation_scripts/inference/segment_loss_ablation/inference_3.sh
+++ training/cogvideox/ablation_scripts/inference/segment_loss_ablation/inference_3.sh
@@ -6,12 +6,12 @@ export TORCH_NCCL_ENABLE_MONITORING=0
 export TOKENIZERS_PARALLELISM=False
 export CUDA_VISIBLE_DEVICES=2
 MODEL_PATH="THUDM/CogVideoX-5b-I2V"
-controlnet_path="checkpoints/box-ablation-segmentloss/checkpoint-3300.pt"
+controlnet_path="checkpoints/box_ablation_segment/checkpoint-3500.pt"
 
 python training/cogvideox/I2V_inference_controlnet.py \
     --pretrained_model_name_or_path  $MODEL_PATH \
     --pretrained_controlnet_path $controlnet_path \
-    --validation_args_csv  "training/cogvideox/validation_args/testset/box_segment_loss_ablation/local_part/final_testset_part_3.csv" \
+    --validation_args_csv  "training/cogvideox/validation_args/DAVIS/box_segment_ablation/remote_machine/step3500.csv" \
     --num_validation_videos 1 \
     --seed 42 \
     --height 480 \
